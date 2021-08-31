1c1d75ff24ec8b5aad0512a127145179521f482c
Morna Rajdeep <morna.rajdeep1991@gmail.com>
2021-09-01 02:52:47 +0400

    home component

---
+++ force-app/main/default/lwc/showupcomingsessions/showupcomingsessions.js
@@ -0,0 +1,14 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import getallSessions from '@salesforce/apex/SessionController.getallSessions';
+export default class Showupcomingsessions extends LightningElement {
+@track sessions=[];
+@wire(getallSessions)
+wiredSession({ error, data }) {
+  if (data) {
+    this.sessions = data;
+  } else if (error) {
+    this.sessions = undefined;
+    throw new Error('Failed to retrieve sessions');
+  }
+}
+}
